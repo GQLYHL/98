e5fd80f66afdb6618afa57fcfe761ff02fd2e802
Administrator <Administrator@JQJLJDG6B4V3H09>
2019-06-28 09:46:44 +0800

    sds

--- sp09-feign/src/main/java/com/tedu/sp09/Sp09FeignApplication.java
+++ sp09-feign/src/main/java/com/tedu/sp09/Sp09FeignApplication.java
@@ -1,5 +1,5 @@
 package com.tedu.sp09;
-//789
+//789sfdsd
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
